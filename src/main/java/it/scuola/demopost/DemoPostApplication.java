79c2360e390b9b2d4f67ca9ca245058c514d4bff
Gianluigi-prof <caddeo.gianluigi@iisbuonarrotivolta.it>
2026-03-25 21:26:41 +0100

    Add files via upload

---
+++ src/main/java/it/scuola/demopost/DemoPostApplication.java
@@ -0,0 +1,13 @@
+package it.scuola.demopost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPostApplication.class, args);
+	}
+
+}
