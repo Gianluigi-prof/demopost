9271c34015338afa6921c97100f6a7f96d3c2f32
Gianluigi-prof <caddeo.gianluigi@iisbuonarrotivolta.it>
2026-05-04 14:28:49 +0000

    Updated with redirection

--- src/main/java/it/scuola/demopost/MessageController.java
+++ src/main/java/it/scuola/demopost/MessageController.java
@@ -1,7 +1,16 @@
 package it.scuola.demopost;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/messages")
@@ -13,24 +22,28 @@ public class MessageController {
         this.repo = repo;
     }
 
-    // GET: leggo tutti i messaggi dal DB
     @GetMapping
     public List<Message> all() {
         return repo.findAll();
     }
 
-    // POST: scrivo un messaggio nel DB
+    // POST JSON: fetch, curl, Postman
     @PostMapping
     public Message create(@RequestBody Message msg) {
         return repo.save(msg);
     }
 
-    // FORM HTML classico
     @PostMapping("/form")
-    public String createFromForm(@RequestParam String text) {
+    public ResponseEntity<Void> createFromForm(@RequestParam String text) {
         Message msg = new Message();
         msg.setText(text);
         repo.save(msg);
-        return "redirect:/index.html";
+
+        // return new RedirectView("index.html");
+
+        return ResponseEntity
+                .status(HttpStatus.SEE_OTHER)
+                .header(HttpHeaders.LOCATION, "/index.html")
+                .build();
     }
 }
